d9baf6caddcbdb0227a662df542da1589625dbc1
Volodymyr Tsiumra <volodymyr.tsiumra@gds.ey.com>
2023-05-26 15:24:24 +0200

    Finish all tasks

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,33 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.Objects;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+        arrayDeque.add(Objects.requireNonNull(firstQueue.poll()));
+        arrayDeque.add(Objects.requireNonNull(firstQueue.poll()));
+        arrayDeque.add(Objects.requireNonNull(secondQueue.poll()));
+        arrayDeque.add(Objects.requireNonNull(secondQueue.poll()));
+
+        boolean firstTurn = true;
+        while (secondQueue.size() != 0) {
+            if (firstTurn) {
+                firstQueue.add(arrayDeque.pollLast());
+                arrayDeque.add(Objects.requireNonNull(firstQueue.poll()));
+                arrayDeque.add(Objects.requireNonNull(firstQueue.poll()));
+                firstTurn = false;
+            } else {
+                secondQueue.add(arrayDeque.pollLast());
+                arrayDeque.add(Objects.requireNonNull(secondQueue.poll()));
+                arrayDeque.add(Objects.requireNonNull(secondQueue.poll()));
+                firstTurn = true;
+            }
+        }
+
+        return arrayDeque;
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -5,5 +5,10 @@ import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        PriorityQueue<String> results = new PriorityQueue<>(new StringReserveComparator());
+        results.comparator();
+        results.addAll(firstList);
+        results.addAll(secondList);
+        return results;
     }
 }
